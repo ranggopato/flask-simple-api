d1e89ca73161d1f8d918d036d77f1ee204d11fe1
ranggopato <patoranggo@gmail.com>
2022-10-19 09:18:00 +0700

    create db mysql

--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 import os
 from src.auth import auth
 from src.cats import cats
-
+from src.database import db
 
 
 def create_app(test_config = None):
@@ -11,9 +11,12 @@ def create_app(test_config = None):
     if test_config is None:
         app.config.from_mapping(
             SECRET_KEY = os.environ.get("SECRET_KEY"),
+            SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DB_URI")
         )
     else:
         app.config.from_mapping(test_config)
+    db.app=app
+    db.init_app(app)
     app.register_blueprint(auth)
     app.register_blueprint(cats)
     @app.get("/")
--- src/database.py
+++ src/database.py
@@ -1,14 +1,29 @@
 from datetime import datetime
+from email.policy import default
 from flask_sqlalchemy import SQLAlchemy
-import datetime
 
 db = SQLAlchemy()
 
-class Cat(db.Model):
-    id = db.column(db.Integer, primary_key = True)
-    catname = db.column(db.String(40), unique = True, nullable = False)
-    email = db.column(db.String(40), unique = True, nullable = False)
-    password = db.column(db.String(40), nullable = False)
-    created_at= db.column(db.DateTime, default = datetime.now())
-    created_at= db.column(db.DateTime, default = datetime.now())
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(40), unique = True, nullable = False)
+    email = db.Column(db.String(40), unique = True, nullable = False)
+    password = db.Column(db.String(40), nullable = False)
+    created_at= db.Column(db.DateTime, default = datetime.now())
+    updated_at= db.Column(db.DateTime, default = datetime.now())
+    cats = db.relationship('Cats', backref = "user")
+    def __repr__(self) -> str:
+        return 'User>>>{self.username}'
+class Cats(db.Model):
+     id = db.Column(db.Integer, primary_key = True)
+     jenis = db.Column(db.Text, nullable = True)
+     warna = db.Column(db.Text, nullable = False)
+     kebiri = db.Column(db.Text, default = 0)
+     user_id = db.Column(db.Integer, db.ForeignKey('user.id') )
+     created_at= db.Column(db.DateTime, default = datetime.now())
+     updated_at= db.Column(db.DateTime, default = datetime.now())
+     
+
+     def __repr__(self) -> str:
+        return 'Cats>>>{self.id}'
 
